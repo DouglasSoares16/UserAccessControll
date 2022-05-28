4fa0315a20f08a49098ad13aaa67a2daefc77a2e
Douglas Soares <douglasmaismcpe@gmail.com>
2022-05-28 15:29:52 -0300

    Created Route of 'Authenticate User'

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
+import { AuthenticateUserController } from "@modules/account/useCases/authenticateUser/AuthenticateUserController";
 import { CreateUserController } from "@modules/account/useCases/createUser/CreateUserController";
 
 const createUserController = new CreateUserController();
+const authenticateUserController = new AuthenticateUserController();
 
 const userRoutes = Router();
 
 userRoutes.post("/", createUserController.handle);
+userRoutes.post("/session", authenticateUserController.handle);
 
 export { userRoutes };
