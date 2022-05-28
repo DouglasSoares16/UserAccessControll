ae4e812cd403b7b79c60b03576752d3c5a77cc07
Douglas Soares <douglasmaismcpe@gmail.com>
2022-05-28 16:15:30 -0300

    Added Role Repository to Container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,10 @@
 import { container } from "tsyringe";
 
 import { PermissionRepository } from "@modules/account/infra/typeorm/implementations/PermissionRepository";
+import { RoleRepository } from "@modules/account/infra/typeorm/implementations/RoleRepository";
 import { UserRepository } from "@modules/account/infra/typeorm/implementations/UserRepository";
 import { IPermissionRepository } from "@modules/account/repositories/IPermissionRepository";
+import { IRoleRepository } from "@modules/account/repositories/IRoleRepository";
 import { IUserRepository } from "@modules/account/repositories/IUserRepository";
 
 container.registerSingleton<IUserRepository>("UserRepository", UserRepository);
@@ -10,3 +12,4 @@ container.registerSingleton<IPermissionRepository>(
   "PermissionRepository",
   PermissionRepository
 );
+container.registerSingleton<IRoleRepository>("RoleRepository", RoleRepository);
