223aec90b3ee0d5271a2b8b09f8f20e756dab3f9
Douglas Soares <douglasmaismcpe@gmail.com>
2022-05-28 16:08:53 -0300

    Added Permission Repository to Container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,6 +1,12 @@
 import { container } from "tsyringe";
 
+import { PermissionRepository } from "@modules/account/infra/typeorm/implementations/PermissionRepository";
 import { UserRepository } from "@modules/account/infra/typeorm/implementations/UserRepository";
+import { IPermissionRepository } from "@modules/account/repositories/IPermissionRepository";
 import { IUserRepository } from "@modules/account/repositories/IUserRepository";
 
 container.registerSingleton<IUserRepository>("UserRepository", UserRepository);
+container.registerSingleton<IPermissionRepository>(
+  "PermissionRepository",
+  PermissionRepository
+);
